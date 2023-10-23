d9efea3c1ada2802d5d8ef6c29cf94d04dd20785
vtharaka <vtharaka@routesme.com>
2023-10-23 07:34:38 +0300

    Feature to implement User CRUD

---
+++ TaskManagementSystem/Controllers/UsersController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TaskManagementSystem.Data;
+using TaskManagementSystem.Data.Entities;
+using TaskManagementSystem.ViewModels;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class UsersController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Users
+        public async Task<ActionResult> Index()
+        {
+            var users = db.Users.Include(u => u.Department);
+            return View(await users.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = await db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name");
+        //    return View();
+        //}
+        public ActionResult Create()
+        {
+            var departments = db.Departments.ToList();
+            var viewModel = new UserCreateViewModel
+            {
+                Departments = new SelectList(departments, "DepartmentID", "Name")
+            };
+
+            return View(viewModel);
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Create([Bind(Include = "UserID,Username,DepartmentID")] User user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Users.Add(user);
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", user.DepartmentID);
+        //    return View(user);
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(UserCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map and save the user to the database.
+                var user = new User
+                {
+                    Username = model.Username,
+                    DepartmentID = model.DepartmentId,
+                    // Add other user properties here.
+                };
+
+                db.Users.Add(user);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index"); // Redirect to a success page.
+            }
+
+            // Model validation failed. Re-populate the department dropdown.
+            var departments = db.Departments.ToList();
+            model.Departments = new SelectList(departments, "DepartmentId", "Name");
+            return View(model); // Return to the form with error messages.
+        }
+
+
+
+
+        // GET: Users/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = await db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", user.DepartmentID);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "UserID,Username,DepartmentID")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "Name", user.DepartmentID);
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = await db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            User user = await db.Users.FindAsync(id);
+            db.Users.Remove(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TaskManagementSystem/ViewModels/UserCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskManagementSystem.ViewModels
+{
+    public class UserCreateViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+
+        public SelectList Departments { get; set; }
+    }
+
+}
